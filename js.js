fd809942e1b4dd8c2f0d9d226dbb4fafcb025d9f
estudantedehtml <mauriciopedeo007@gmail.com>
2022-07-20 23:50:15 -0300

    site-teste

---
+++ js.js
@@ -0,0 +1,27 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+
+  
+  const active = nav.classList.contains('active');
+
+  const off = document.getElementById('section');
+  off.classList.toggle('off');  
+  
+  const scroll = document.getElementById('scroll');
+  scroll.classList.toggle('scroll');
+
+
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
